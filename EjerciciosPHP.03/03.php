a47a9bdbf858eef7603c0be11af5b855f0d515b8
Ylich Layana <ylichlayana@gmail.com>
2021-10-04 13:37:08 +0200

    En este ejercicio 03.php esta basado en en ejercicio anterior de Diarios uso la misma estructura del array pero en este cao escogere un diario al azar, para esto he usado una funcion predefinidad de PHP la cual es array_rand la que me da una clave del array

---
+++ EjerciciosPHP.03/03.php
@@ -0,0 +1,33 @@
+<?php
+
+function ListaDiarios()
+{
+    $diarios = array(
+        'Marca' => "https://www.marca.com",
+        'EL Pais' => '"https://www.elpais.com"',
+        'El Mundo' => "https://www.elmundo.es",
+        'La Vanguardia' => 'https://www.lavanguardia.com/',
+        "La Razón" => "https://www.larazon.es"
+    );
+    
+    
+    $claveDiario = array_rand($diarios);
+	echo "<h4>El Medio recomendado es: <a href=\"". $diarios[$claveDiario]. "\">$claveDiario</a></h4>";
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>Diario Aleatorio </h2>
+    <?= ListaDiarios() ?>
+</body>
+</html>
