ddd91d812ee3f368450be37f8ef5898045167475
YlichLayana <ylichlayana@gmail.com>
2022-05-30 23:40:05 +0200

    He realizado cambios en el models de Categoria.php donde he mejorado la funcion que me muestra todos las categorias que haya en la bdd con mas datos pedido en relacion a lo solicitado en el proyecto, tambien he agregado las funciones para editar y elimininar categorias

--- TProyectos/master-php/models/Categoria.php
+++ TProyectos/master-php/models/Categoria.php
@@ -25,8 +25,14 @@ class Categoria{
 		$this->nombre = $this->db->real_escape_string($nombre);
 	}
 
+	//He realizado cambios en esta funcion para poder obtener todos los dato solicitado en el crud
 	public function getAll(){
-		$categorias = $this->db->query("SELECT * FROM categorias ORDER BY id DESC;");
+		
+		/*$categorias=$this->db->query("SELECT c.id, c.nombre, p.categoria_id, sum(p.stock) AS stock, sum(p.precio) AS total FROM categorias c, productos p WHERE c.id=p.categoria_id GROUP by c.id ORDER BY c.id DESC;");*/
+		
+		$categorias = $this->db->query("SELECT c.id, c.nombre, (SELECT sum(stock) FROM productos WHERE categoria_id = c.id) AS 'stock', (SELECT sum(precio) FROM productos WHERE categoria_id = c.id) AS 'total' FROM categorias c ORDER by id DESC;");
+
+		
 		return $categorias;
 	}
 	
@@ -46,5 +52,33 @@ class Categoria{
 		return $result;
 	}
 	
+	//agregue funcion editar categoria
+	public function edit(){
+		$sql = "UPDATE categorias SET nombre='{$this->getNombre()}' ";
+
+		$sql .= " WHERE id={$this->id};";
+		
+		$save = $this->db->query($sql);
+		
+		$result = false;
+		if($save){
+			$result = true;
+		}
+		return $result;
+	}
+
+	//Tambien he agregado esta funcion para poder borrar un dato de categoria de la tabla o bbdd
+	public function delete(){
+		$sql = "DELETE FROM categorias WHERE id={$this->id}";
+		$delete = $this->db->query($sql);
+		
+		$result = false;
+		if($delete){
+			$result = true;
+		}
+		return $result;
+	}
+
+
 	
 }
